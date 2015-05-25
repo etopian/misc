d0e839f18d5475daea6bd92f920200b442502ed5
Etopian <sami@etopian.net>
2015-05-25 05:50:16 -0600

    Update fixperms.sh

--- fixperms.sh
+++ fixperms.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-s3cmd ls --recursive s3://manager.dockerwordpress.com | grep \\.html | cut -c 30- | xargs s3cmd modify --add-header=Cache-Control:max-age=86400 -m text/html
+# sed 's/^/\"/; s/$/\"/;'    <- this will quote the line
+s3cmd ls --recursive s3://manager.dockerwordpress.com | grep \\.html | cut -c 30- | sed 's/^/\"/; s/$/\"/;' | xargs s3cmd modify --add-header=Cache-Control:max-age=86400 -m text/html
 
 s3cmd ls --recursive s3://manager.dockerwordpress.com | grep \\.css | cut -c 30- | xargs s3cmd modify --add-header=Cache-Control:max-age=86400 -m text/css
 
