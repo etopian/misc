d18755fb8c3a1b136dc9cc30f687fa5cb8a07ff3
Sami <sami@etopian.net>
2015-07-31 18:33:40 -0600

    fix actually saving as png rather than svg.

--- svgconvert.sh
+++ svgconvert.sh
@@ -1,3 +1,3 @@
 for i in *eps; do inkscape $i --export-plain-svg=$i.svg  -h512 -w512; done
-for i in *svg; do rsvg-convert -a -w 512 -f svg $i -o $i.svg; done
+for i in *svg; do rsvg-convert -a -w 512 -f png $i -o $i.png; done
 rename s/.eps.svg// *svg
